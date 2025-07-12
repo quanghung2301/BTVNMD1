beee7ffb2fc094d69d2e5c56eefbec6dfee1e167
quanghung2301 <cawcao613@gmail.com>
2025-07-12 14:28:41 +0900

    Create Ex4.java

---
+++ java/SS1/out/production/SS1/.idea/Ex4.java
@@ -0,0 +1,26 @@
+
+
+import java.util.Scanner;
+
+public class Ex4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int age = 0;
+
+        do {
+            System.out.print("Nhập vào tuổi của bạn: ");
+            if (scanner.hasNextInt()) {
+                age = scanner.nextInt();
+                if (age <= 0) {
+                    System.out.println("Vui lòng nhập vào một số nguyên và lớn hơn 0.");
+                }
+            } else {
+                System.out.println("Vui lòng nhập vào một số nguyên và lớn hơn 0.");
+                scanner.next();
+            }
+        } while(age <= 0);
+
+        System.out.printf("Tuổi của bạn là %d !%n", age);
+        scanner.close();
+    }
+}
